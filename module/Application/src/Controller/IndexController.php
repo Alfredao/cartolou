7ac0cf2695e45baaf98072ed3f6a9b634245d06d
Alfredo Costa <alfredocosta@live.com>
2016-06-30 21:13:36 -0300

    -

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -22,6 +22,49 @@ class IndexController extends AbstractActionController
 		$container = new \Zend\Container\Session();
 		$slug = 'bbx-champions-league';
 		
+		
+		// LOGIN
+		header('Access-Control-Allow-Origin: *');  
+		header('Content-type: application/json');
+
+		if (! $container->offSetExists('glbId')) {			
+			$email = getenv('email');
+			$password = getenv('senha');
+			$serviceId = 4728;
+
+			$url = 'https://login.globo.com/api/authentication';
+
+			$jsonAuth = array(
+				'payload' => array(
+				  'email' => $email,
+				  'password' => $password,
+				  'serviceId' => $serviceId
+				)
+			);
+
+			$c = curl_init();
+			curl_setopt($c, CURLOPT_URL, $url);
+			curl_setopt($c, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+			curl_setopt($c, CURLOPT_POST, true);
+			curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($jsonAuth));
+
+			$result = curl_exec($c);
+
+			if ($result === FALSE) {
+				die(curl_error($c));
+			} else {
+			  //echo $result;
+			}
+
+			curl_close($c);
+
+			$parseJson = json_decode($result, TRUE);
+			$container->glbId = $parseJson['glbId'];
+			
+		}
+		
 		// $orderBy: campeonato, turno, mes, rodada, patrimonio
 		$orderBy = "";
 		//if (isset($_GET["orderBy"]) && $_GET["orderBy"] != "") {
